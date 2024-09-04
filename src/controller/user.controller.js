0ae3226fa665c818d14e08c4c223136f48c292cc
dilshan.d3 <dilshan72404@gmail.com>
2024-09-04 17:53:08 +0530

    debuggin

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,21 +4,6 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnClodinary } from "../utils/cloudinary.js";
 
-// const generateAccessAndRefreshTokens = async (userId) => {
-//     try {
-//         const user = await User.findById(userId)
-//         const accessToken = await user.generateAccessToken()
-//         const refreshToken = await user.generateRefreshToken()
-
-//         user.refreshToken = refreshToken
-//         await user.save({ validateBeforeSave: false })
-
-//         return { accessToken, refreshToken }
-
-//     } catch (error) {
-//         throw new ApiError(500, "Unable to generate access and refresh token")
-//     }
-// }
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
       const user = await User.findById(userId)
